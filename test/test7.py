1de41e17a6998439fc29ec3e9593f22c7e853c9e
RiDoX-i <your.email@example.com>
2026-04-08 12:45:52 +0200

    corrected test7

--- test/test7.py
+++ test/test7.py
@@ -7,8 +7,8 @@ if str(ROOT) not in sys.path:
 
 from Code.turing_machine import coder_machine_q7
 
-
 def test_question7_minimal():
-    resultat = coder_machine_q7("test/q7.tms")
+    chemin_tms = Path(__file__).resolve().parent / "q7.tms"
+    resultat = coder_machine_q7(str(chemin_tms))
     attendu = "0|0|1|>|1|0|□|□|-|1"
     assert resultat == attendu
