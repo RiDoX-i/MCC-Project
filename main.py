f31c21bf50f7bceb4e0e2e778b89accdb946971b
RiDoX-i <your.email@example.com>
2026-04-08 12:02:53 +0200

    update

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from Code.turing_machine import configuration_initiale, executer_un_pas, lire_machine_tms
 
+# garder le code et ajouter du code pour executer la question faite
 
 if __name__ == "__main__":
+    
     machine = lire_machine_tms("test/q2.tms")
     config = configuration_initiale(machine, "0")
     fait = executer_un_pas(machine, config)
